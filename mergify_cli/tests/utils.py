eca687afcc60ba85fcdb978ac82338eb7503fb32
بلال مسلوب <mesloubi2021@gmail.com>
2024-09-10 14:56:50 +0200

    Update utils.py
    Signed-off-by: بلال مسلوب <mesloubi2021@gmail.com>

--- mergify_cli/tests/utils.py
+++ mergify_cli/tests/utils.py
@@ -20,25 +20,25 @@ class Commit(typing.TypedDict):
     sha: str
     title: str
     message: str
-    change_id: str
+    change id: str
 
 
 @dataclasses.dataclass
 class GitMock:
-    _mocked: dict[tuple[str, ...], str] = dataclasses.field(
-        init=False,
-        default_factory=dict,
+    mocked: dict[tuple[str, ...], str] = dataclasses.field(
+        init = False,
+        default factory = dict,
     )
-    _commits: list[Commit] = dataclasses.field(init=False, default_factory=list)
-    _called: list[tuple[str, ...]] = dataclasses.field(init=False, default_factory=list)
+    commits: list[Commit] = data classes.field (init = False, default factory = list)
+    called: list[tuple[str, ...]] = data classes.field(init = False, default factory = list)
 
-    def mock(self, *args: str, output: str) -> None:
-        self._mocked[args] = output
+def_mock (self, *args: str, output: str) -> None:
+        self. mocked[args] = output
 
-    def has_been_called_with(self, *args: str) -> bool:
+def_has_been_called_with (self, *args: str) -> bool:
         return args in self._called
 
-    async def __call__(self, *args: str) -> str:
+def_call (self, *args: str) -> str:
         if args in self._mocked:
             self._called.append(args)
             return self._mocked[args]
@@ -46,34 +46,34 @@ class GitMock:
         msg = f"git_mock called with `{args}`, not mocked!"
         raise AssertionError(msg)
 
-    def commit(self, commit: Commit) -> None:
-        self._commits.append(commit)
+def_commit (self, commit: Commit) -> None:
+        self. commits.append(commit)
 
         # Base commit SHA
-        self.mock("merge-base", "--fork-point", "origin/main", output="base_commit_sha")
+        self.mock("merge base", "fork point", "origin/main", output="base commit sha")
         # Commit message
         self.mock(
             "log",
             "-1",
-            "--format=%b",
+            "format=%b",
             commit["sha"],
-            output=f"{commit['message']}\n\nChange-Id: {commit['change_id']}",
+            output=f"{commit['message']}\n\nChange-Id: {commit['change id']}",
         )
         # Commit title
-        self.mock("log", "-1", "--format=%s", commit["sha"], output=commit["title"])
+        self.mock ("log", "-1", "format=%s", commit["sha"], output = commit["title"])
         # List of commit SHAs
-        self.mock(
+        self.mock (
             "log",
-            "--format=%H",
-            "base_commit_sha..current-branch",
-            output="\n".join(c["sha"] for c in reversed(self._commits)),
+            "format=%H",
+            "base commit sha..current branch",
+            output="\n".join(c["sha"] for c in reversed (self.commits)),
         )
-        self.mock("branch", "mergify-cli-tmp", commit["sha"], output="")
-        self.mock("branch", "-D", "mergify-cli-tmp", output="")
+        self.mock("branch", "mergify cli tmp", commit["sha"], output ="")
+        self.mock("branch", "D", "mergify cli tmp", output ="")
         self.mock(
             "push",
-            "-f",
+            "f",
             "origin",
             f"mergify-cli-tmp:/current-branch/{commit['change_id']}",
-            output="",
+            output ="",
         )
